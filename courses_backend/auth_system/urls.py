443851cda723db28e81f74240ed4ad34fb6ffcba
ENTITY514 <entity514@mail.ru>
2024-04-20 20:22:07 +0500

    user registration add

--- courses_backend/auth_system/urls.py
+++ courses_backend/auth_system/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from courses_backend.auth_system.views import UserRegistration
 
 urlpatterns = [
-    path('authorization', UserRegistration)
+    path('registration', UserRegistration)
 ]
--- courses_backend/auth_system/views.py
+++ courses_backend/auth_system/views.py
@@ -1,8 +1,30 @@
+import json
 from django.contrib.auth.models import User
+from django.http import JsonResponse
 
-def UserRegistration():
-    user = User.objects.create_user('myusername', 'myemail@crazymail.com', 'mypassword')
 
-    user.first_name = 'John'
-    user.last_name = 'Citizen'
-    user.save()
+def UserRegistration(request):
+
+    json_response = {
+        "status":"no_status",
+        "errors":"no_errors",
+    }
+
+    json_data = request.POST.get('json_data')
+    data = json.loads(json_data)
+
+    #Проверка сушествования пользователя
+    mail_exist = User.objects.filter(email=data["email"]).exists()
+
+    if mail_exist:
+        json_response["status"] = "failed"
+        json_response["errors"] = "Пользователь с такой почтой уже существует"
+    else:
+        user = User.objects.create_user(data["email"], data["email"], data["password"])
+
+        user.first_name = data["firstname"]
+        user.last_name = data["lastname"]
+        user.save()
+        json_response["status"] = "succesfully"
+
+    return JsonResponse(json_response)
